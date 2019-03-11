b6b0901a6fe6662a3351fb36ea6bc81cce57cf3c
Alan Jachimczak <S00168420@mail.itsligo.ie>
2019-03-11 15:28:22 +0000

    Added TopicRepository

---
+++ NoodleProject.WebApi/Models/Repositories/ITopicRepository.cs
@@ -0,0 +1,13 @@
+﻿using NoodleProject.WebApi.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoodleProject.WebApi.Models.Repositories
+{
+    public interface ITopicRepository: IRepository<Topic, int>
+    {
+        IEnumerable<Topic> getAllForUserId(string UserId);
+    }
+}
